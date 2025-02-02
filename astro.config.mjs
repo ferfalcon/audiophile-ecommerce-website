e3f43e1af54001932c2423cdfe29c17fd1d2be65
Fer <fer.falcon.dg@gmail.com>
2025-02-02 20:03:43 -0300

    Vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
+
+ 
 export default defineConfig({
   output: 'server',
   adapter: vercel({
